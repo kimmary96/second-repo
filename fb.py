49eb790cf202c48c51ad726d382fbfcbc7ab23f1
kimmary96 <kimgone125@gmail.com>
2025-12-30 15:35:08 +0900

    feat: Add fizzbuzz

--- fb.py
+++ fb.py
@@ -1,7 +1,9 @@
 for i in range(1, 15 + 1):
     if i % 3 == 0:
-        print("fuzz")
+        print("fizz")
     elif i % 5 == 0:
         print("buzz")
+    elif i % 15 == 0:
+        print("fizzbuzz")
     else:
         print(i)
