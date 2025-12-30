97c22fd2ba7946b7f3cc134c8698cca4b3563f27
kimmary96 <kimgone125@gmail.com>
2025-12-30 15:39:16 +0900

    feat: Refactor

--- fb.py
+++ fb.py
@@ -1,7 +1,5 @@
 for i in range(1, 15 + 1):
-    if i % 3 == 0:
-        print("fuzz")
-    elif i % 5 == 0:
-        print("buzz")
+    if i % 3 == 0 or i % 5 == 0:
+        print("fizz" * (i % 3 == 0) + "buzz" * (i % 5 == 0))
     else:
         print(i)
